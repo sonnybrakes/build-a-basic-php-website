d7ca2bb008222f1100c0a559faa62cd297c4a8b9
Michel Frechette <mifrec@hotmail.com>
2016-03-24 06:49:38 -0400

    Including the Footer and Adding Additional Pages

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include("inc/header.php"); ?>
-		<div class="section catalog random">
+		<div class="section catalogue random">
 
 			<div class="wrapper">
 
@@ -34,21 +34,4 @@
 		</div>
 
 	</div><!-- end content -->
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
-
-		</div>
-
-	</div>
-
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
