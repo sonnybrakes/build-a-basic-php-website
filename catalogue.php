fb0f7bafe7781babf83bf1b10fbc13b31aa1f5a7
Michel Frechette <mifrec@hotmail.com>
2016-03-24 08:41:45 -0400

    Working with _GET Variables

--- catalogue.php
+++ catalogue.php
@@ -1,10 +1,19 @@
 <?php
 $pageTitle = "Full Catalogue";
 
+if (isset($_GET["cat"])) {
+  if ($_GET["cat"] == "books") {
+    $pageTitle = "Books";
+  } else if ($_GET["cat"] == "movies") {
+    $pageTitle = "Movies";
+  } else if ($_GET["cat"] == "music") {
+    $pageTitle = "Music";
+  }
+}
 include("inc/header.php"); ?>
 
 <div class="section page">
-  <h1>Full Catalogue</h1>
+  <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
--- inc/header.php
+++ inc/header.php
@@ -12,9 +12,9 @@
 			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
 
 			<ul class="nav">
-                <li class="books"><a href="catalogue.php">Books</a></li>
-                <li class="movies"><a href="catalogue.php">Movies</a></li>
-                <li class="music"><a href="catalogue.php">Music</a></li>
+                <li class="books"><a href="catalogue.php?cat=books">Books</a></li>
+                <li class="movies"><a href="catalogue.php?cat=movies">Movies</a></li>
+                <li class="music"><a href="catalogue.php?cat=music">Music</a></li>
                 <li class="suggest"><a href="suggest.php">Suggest</a></li>
             </ul>
 
