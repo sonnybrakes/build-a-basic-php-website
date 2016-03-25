1ff20d0ed252442136c90b81ff0a9f35ee8ff427
Michel Frechette <mifrec@hotmail.com>
2016-03-25 16:21:21 -0400

    Creating the Catalog Array

--- catalogue.php
+++ catalogue.php
@@ -1,4 +1,9 @@
 <?php
+$catalogue = array(
+  "Design Patterns",
+  "Forrest Gump",
+  "Beethovan"
+);
 $pageTitle = "Full Catalogue";
 $section = null;
 if (isset($_GET["cat"])) {
@@ -15,8 +20,18 @@ if (isset($_GET["cat"])) {
 }
 include("inc/header.php"); ?>
 
-<div class="section page">
-  <h1><?php echo $pageTitle; ?></h1>
+<div class="section catalogue page">
+
+  <div class="wrapper">
+    <h1><?php echo $pageTitle; ?></h1>
+    <ul>
+      <?php foreach($catalogue as $item) {
+        echo "<li>" . $item . "</li>";
+      }
+      ?>
+    </ul>
+  </div>
+
 </div>
 
 <?php include("inc/footer.php"); ?>
